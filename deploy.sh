379df9f0650daf984061ec76ae0c265e57a66331
hoey194 <377954575@qq.com>
2022-12-28 23:06:04 +0800

    适配hexo deploy plugin 多git租户

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,10 @@ cd /Users/hoey/workspace/hexo/blog-src/note
 hexo clean
 hexo generate
 echo "note.hoey.tk" > /Users/hoey/workspace/hexo/blog-src/note/public/CNAME
+
+git config --global user.name "hoey194"
+git config --global user.email "377954575@qq.com"
 hexo deploy
+
+git config --global --unset user.name
+git config --global --unset user.email
