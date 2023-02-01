55c1c2c8cf27b2f9bd87f296871ca90cb46a3b1f
hoey194 <377954575@qq.com>
2023-02-01 10:41:39 +0800

    自动创建sage隐藏文件

---
+++ new_sage.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+# 在sage目录下创建文章
+
+title=$1
+
+if [ "${title}" == "吉事语" ]; then
+  title=$title`date +%y%m%d`
+fi
+
+hexo new post "\"${title}\""
+mv /Users/hoey/workspace/hexo/blog-src/note/source/_posts/${title}.md /Users/hoey/workspace/hexo/blog-src/note/source/sage/${title}.md
+
+
